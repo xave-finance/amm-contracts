4c65c088e80b98ba577fc311c4917a982f6a87cf
James Russell Orola <jrmorola@gmail.com>
2022-04-27 19:58:32 +0800

    Reuse swap lib on fx-pool deploy

--- scripts/utils/addresses.ts
+++ scripts/utils/addresses.ts
@@ -32,23 +32,26 @@ export const getTokenOracleAddress = (network: string, baseToken: string) => {
 }
 
 export const getAssimilatorFactoryAddress = (network: string) => {
-  const haloAddresses = getHaloAddresses(network)
-  if (!haloAddresses) return undefined
-  return haloAddresses.ammV2.assimilatorFactory
+  return '0x716f4B41d73447c5e2576AaC27Fb55587B5D5337'
+  // const haloAddresses = getHaloAddresses(network)
+  // if (!haloAddresses) return undefined
+  // return haloAddresses.ammV2.assimilatorFactory
 }
 
 export const getProportionalLiquidityAddress = (network: string) => {
-  const haloAddresses = getHaloAddresses(network)
-  if (!haloAddresses) return undefined
-  return haloAddresses.ammV2.proportionalLiquidity
+  return '0x72a42b82cC68f5d4eC5767C653303aCD2863B71d'
+  // const haloAddresses = getHaloAddresses(network)
+  // if (!haloAddresses) return undefined
+  // return haloAddresses.ammV2.proportionalLiquidity
 }
 
 // need to add haloAddresses.ammV2.swapLib to @halodao/halodao-contract-addresses package
-// export const getSwapLibAddress = (network: string) => {
-//   const haloAddresses = getHaloAddresses(network)
-//   if (!haloAddresses) return undefined
-//   return haloAddresses.ammV2.swapLib
-// }
+export const getSwapLibAddress = (network: string) => {
+  return '0x49d71B23C57afA2365200A2c1f344d14e93164DE'
+  // const haloAddresses = getHaloAddresses(network)
+  // if (!haloAddresses) return undefined
+  // return haloAddresses.ammV2.swapLib
+}
 
 export const getVaultAddress = (network: string) => {
   const haloAddresses = getHaloAddresses(network)
